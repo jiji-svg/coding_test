fea07ef4de52cc9ab87ede032c7e74c96249ef57
jiji-svg <minemine535@gmail.com>
2024-12-08 17:27:47 +0900

    feat: make list with arr according to flag if flag[i] is true add arr[i], arr[i]*2 times else remove arr[i] times from the last unit

---
+++ level0/day7/add_del_list.py
@@ -0,0 +1,13 @@
+def solution(arr, flag):
+    answer = []
+    for i in range(len(arr)):
+        if flag[i]:
+            for _ in range(arr[i]*2):
+                answer.append(arr[i])
+        else:
+            for _ in range(arr[i]):
+                answer.pop()
+                if answer == []:
+                    break
+            
+    return answer
