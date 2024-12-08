b88a57d2f2095dd23c2e008362e561e17576c83e
jiji-svg <minemine535@gmail.com>
2024-12-08 17:37:07 +0900

    feat: split with blanks

---
+++ level0/day7/recogize_with_blank.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+#     my_string = my_string.strip()
+    
+
+#     solution=lambda x:x.split()
+    answer = my_string.split()
+    # answer = [ans for ans in my_string.split(' ') if ans]
+    # for ans in tmp_answer
+    # answer = answer.strip()
+    return answer
