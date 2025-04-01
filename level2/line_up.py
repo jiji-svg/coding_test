5e6bd1c050c1e83bd42231dfbae5f02905122d56
jiji-svg <minemine535@gmail.com>
2025-04-01 11:20:45 +0900

    feat: there's n people with number code n they can line up in different ways find the kth way when all the ways are listed and when it's lined by dictionary order

---
+++ level2/line_up.py
@@ -0,0 +1,9 @@
+def solution(n, k):
+    from math import factorial
+    answer = []
+    order = list(range(1,n+1))
+    while n!=0 :
+        fact = factorial(n-1)
+        answer.append(order.pop((k-1)//fact))
+        n,k = n-1, k%fact
+    return answer
